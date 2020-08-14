f77ab2a7b246bb82b82a5a1d4ffbeed1b749d193
rayleigh18 <sitanggangirfan271000@gmail.com>
2020-08-14 19:28:34 +0700

    add device width

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import RegisPage from "./Stateless/Regis/RegisPage";
+import RegisPage from "./Stateless/Regis/RegisPage"
 
 
 export default function App() {
   return (
-    <>
-      <RegisPage/>
-    </>
+    <View style={styles.container}>      
+      <RegisPage />
+      
+    </View>
   );
 }
 
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f9f9f9',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
--- Stateless/Regis/RegisPage.tsx
+++ Stateless/Regis/RegisPage.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import device from "../../device"
 
 const RegisPage = () =>(
-    <Text>
-        Ayam
-    </Text>
+    <View style = {styles.container}>
+        <Text style = {styles.judul}>
+            Registration
+        </Text>    
+    </View>
+    
 )
+
+const styles = StyleSheet.create({
+    judul : {
+        color : '#278CA1',
+        fontSize : 46,
+        marginLeft : 15*(device.Width)/360,
+    },
+    container : {
+        backgroundColor : '#f9f9f9',
+    }
+})
+
 
 
 export default RegisPage;
