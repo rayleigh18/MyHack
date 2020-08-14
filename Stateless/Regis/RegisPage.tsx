6cbce2e1cdbaec8a6e18131dec2a37edfb4cd428
rayleigh18 <sitanggangirfan271000@gmail.com>
2020-08-14 19:01:38 +0700

    add Assets

---
+++ Stateless/Regis/RegisPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const RegisPage = () =>(
+    <Text>
+        Ayam
+    </Text>
+)
+
+
+export default RegisPage;
